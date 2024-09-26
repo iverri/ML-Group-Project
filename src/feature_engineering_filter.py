291c7ec7c356bc26b6db226c23460fe0b52c8556
tobiasfremming <tobias.fremming@gmail.com>
2024-09-26 12:44:35 +0200

    feat: add skeleton for feature_engineering filters

---
+++ src/feature_engineering_filter.py
@@ -0,0 +1,18 @@
+from src.preprocess_filter import Filter
+
+
+class Feature_engineering_excample(Filter):
+    """Feature_engineering_excample is a filter class that does some feature engineering
+    """
+    def __call__(self, data):
+        """_summary_ apply the preprocessing filter to the data, so that the filter is callable. 
+
+        Args:
+            data (_type_): _description_ the data to be sorted
+            
+        Returns:
+            _type_:? _description_ the sorted data
+        """
+        
+        #TODO: implement the feature engineering
+        pass
